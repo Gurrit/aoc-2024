fe71350ff5ff4ffeb07d5dec219acb73299a629d
Gustav Engsmyre <gustav.engsmyre@pagero.com>
2024-12-03 14:38:58 +0100

    Add Day3

--- src/main/scala/day/day03/Part1.scala
+++ src/main/scala/day/day03/Part1.scala
@@ -2,10 +2,20 @@ package day.day03
 
 import day.Part
 
+import scala.util.chaining.scalaUtilChainingOps
+import scala.util.matching.Regex
+
 object Part1 extends Part {
-  
+
+  private val mulRegex: Regex = """mul\((\d+),(\d+)\)""".r
+
+
   def solve(input: Seq[String]): Unit = {
-    
+    val foundRegex = mulRegex.findAllMatchIn(input.mkString)
+    val multiplications = foundRegex.map(_.subgroups.pipe(s => (s.head.toLong, s.last.toLong))).toSeq
+    val res = multiplications.map(m => m._1 * m._2)
+
+    println(res.sum)
   }
 
 }
--- src/main/scala/day/day03/Part2.scala
+++ src/main/scala/day/day03/Part2.scala
@@ -2,10 +2,34 @@ package day.day03
 
 import day.Part
 
-object Part2 extends Part {
-  
-  def solve(input: Seq[String]): Unit = {
-    
-  }
+import scala.util.chaining.scalaUtilChainingOps
+import scala.util.matching.Regex
 
-}
+object Part2 extends Part {
+
+    private val mulRegex: String = """mul\((\d+),(\d+)\)"""
+    private val doRegex: String = """(do\(\))"""
+    private val dontRegex: String = """(don\'t\(\))"""
+
+
+    def solve(input: Seq[String]): Unit = {
+      val fullRegex = s"$mulRegex|$doRegex|$dontRegex".r
+
+      val foundOps = fullRegex.findAllMatchIn(input.mkString).toSeq
+      val res = foundOps.map(_.subgroups).foldLeft((0, true)) { (acc, op) =>
+        op match {
+          case Seq(a, b, null, null) if acc._2 => {
+            (acc._1 + a.toInt * b.toInt, acc._2)
+          }
+          case Seq(null, null, _, null) => (acc._1, true)
+          case Seq(null, null, null, _) => (acc._1, false)
+          case _ => acc
+        }
+      }
+      println(res._1)
+
+    }
+
+
+
+  }
